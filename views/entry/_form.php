a506d7060ec51646b581ff87fc3d9d7046bf1cc3
staxDB <borndavid508@gmail.com>
2017-11-27 17:26:46 +0100

    better create/update fields (DateTimePicker & Checkbox for all_day

--- views/entry/_form.php
+++ views/entry/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\widgets\DateTimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model humhub\modules\calendar_extension\models\CalendarExtensionCalendarEntry */
@@ -12,21 +13,20 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'uid')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'uid')->textInput(['maxlength' => true, 'disabled' => true]) ?>
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'start_datetime')->textInput() ?>
-<!--    --><?//=
-//    $form->field($model, 'dtstamp')->widget(DateTimePicker::className(), ['type' => DateTimePicker::TYPE_COMPONENT_APPEND, 'pluginOptions' => ['format' => 'yyyy-mm-dd hh:ii:ss', 'autoclose' => false]])
-//    ?>
+    <?= $form->field($model, 'location')->textarea(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'start_datetime')->widget(DateTimePicker::className(), ['type' => DateTimePicker::TYPE_COMPONENT_APPEND, 'pluginOptions' => ['format' => 'yyyy-mm-dd hh:ii:ss', 'autoclose' => false]]) ?>
 
-    <?= $form->field($model, 'end_datetime')->textInput() ?>
+    <?= $form->field($model, 'end_datetime')->widget(DateTimePicker::className(), ['type' => DateTimePicker::TYPE_COMPONENT_APPEND, 'pluginOptions' => ['format' => 'yyyy-mm-dd hh:ii:ss', 'autoclose' => false]]) ?>
 
-    <?= $form->field($model, 'all_day')->textInput() ?>
+    <?= $form->field($model, 'all_day')->checkbox() ?>
+
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('base', 'Create') : Yii::t('base', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
