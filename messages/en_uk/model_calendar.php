19deeadb13a96bc8fa18078e4d6f27b121e6d272
staxDB <borndavid508@gmail.com>
2017-12-05 20:21:35 +0100

    added en-uk messages

---
+++ messages/en_uk/model_calendar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davidborn
+ * Date: 05.12.17
+ * Time: 10:28
+ */
+return array(
+    'ID' => '',
+    'Title' => '',
+    'Url' => '',
+    'Public' => '',
+    'Timezone' => '',
+    'Color' => '',
+    'iCal Version' => '',
+    'Original Calendar Name' => '',
+    'Original Calendar Scale' => '',
+);
---
+++ messages/en_uk/sync_result.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'Sync successfull!' => '',
+    'Calendar not found!' => '',
+    'Error while check and submit models...' => '',
+    'Error while creating ical... Check if link is reachable.' => '',
+    'No valid ical url! Try an url with http / https.' => '',
+);
